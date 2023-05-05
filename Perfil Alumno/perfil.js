897d256647dfb37ae68702550f3152d0f3714d2c
JoseCortesG <josecortesalc@gmail.com>
2023-05-05 23:35:03 +0200

    FIN VIERNES NOCHE

---
+++ Perfil Alumno/perfil.js
@@ -0,0 +1,45 @@
+function toggle() {
+    var contenido = document.getElementById("tabla");
+    if (contenido.style.width === "45%") {
+      contenido.style.width = "65%";
+    } else {
+      contenido.style.width = "45%";
+    }
+    if (contenido.style.marginLeft === "26%") {
+      contenido.style.marginLeft = "10%";
+    } else {
+      contenido.style.marginLeft = "26%";
+    }
+    if (contenido.style.marginTop === "7%") {
+      contenido.style.marginTop = "5%";
+    } else {
+      contenido.style.marginTop = "7%";
+    }
+  
+    if (contenido.style.height === "65%") {
+      contenido.style.height = "75%";
+    } else {
+      contenido.style.height = "65%";
+    }
+  
+    var contenido = document.getElementById("menu-footer");
+    if (contenido.style.marginRight === "3%") {
+      contenido.style.marginRight = "13%";
+    } else {
+      contenido.style.marginRight = "3%";
+    }
+    var contenido = document.getElementById("nombre");
+    if (contenido.style.marginRight === "7%") {
+      contenido.style.marginRight = "15%";
+    } else {
+      contenido.style.marginRight = "7%";
+    }
+  
+  
+  
+  
+    
+  
+  
+  
+  }

33032f7affd5a5645ba837a5fa8489d89d583b3d
JoseCortesG <josecortesalc@gmail.com>
2023-05-08 00:18:32 +0200

    fin domingo noche

--- Perfil Alumno/perfil.js
+++ Perfil Alumno/perfil.js
@@ -1,45 +1,681 @@
-function toggle() {
-    var contenido = document.getElementById("tabla");
-    if (contenido.style.width === "45%") {
-      contenido.style.width = "65%";
-    } else {
-      contenido.style.width = "45%";
-    }
-    if (contenido.style.marginLeft === "26%") {
-      contenido.style.marginLeft = "10%";
-    } else {
-      contenido.style.marginLeft = "26%";
-    }
-    if (contenido.style.marginTop === "7%") {
-      contenido.style.marginTop = "5%";
-    } else {
-      contenido.style.marginTop = "7%";
-    }
-  
-    if (contenido.style.height === "65%") {
-      contenido.style.height = "75%";
-    } else {
-      contenido.style.height = "65%";
-    }
-  
-    var contenido = document.getElementById("menu-footer");
-    if (contenido.style.marginRight === "3%") {
-      contenido.style.marginRight = "13%";
-    } else {
-      contenido.style.marginRight = "3%";
-    }
-    var contenido = document.getElementById("nombre");
-    if (contenido.style.marginRight === "7%") {
-      contenido.style.marginRight = "15%";
-    } else {
-      contenido.style.marginRight = "7%";
-    }
-  
-  
-  
-  
-    
-  
-  
-  
+
+
+function deslizar() {
+  var contenido = document.getElementById("media-porcentaje-dos");
+  if (contenido.style.transform === "translateX(18rem)") {
+    contenido.style.transform = "translateX(0rem)";
+  } else {
+    contenido.style.transform = "translateX(18rem)";
   }
+
+
+}
+
+
+function toggle() {
+  ///////TABLA/////////////
+  var contenido = document.getElementById("tabla");
+  if (contenido.style.width === "45%") {
+    contenido.style.width = "65%";
+  } else {
+    contenido.style.width = "45%";
+  }
+  if (contenido.style.marginLeft === "26%") {
+    contenido.style.marginLeft = "10%";
+  } else {
+    contenido.style.marginLeft = "26%";
+  }
+  if (contenido.style.marginTop === "7%") {
+    contenido.style.marginTop = "5%";
+  } else {
+    contenido.style.marginTop = "7%";
+  }
+
+  if (contenido.style.height === "65%") {
+    contenido.style.height = "75%";
+  } else {
+    contenido.style.height = "65%";
+  }
+
+
+
+
+  //////////FOOTER////////////////////
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.width === "40%") {
+    contenido.style.width = "60%";
+  } else {
+    contenido.style.width = "40%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.height === "60%") {
+    contenido.style.height = "80%";
+  } else {
+    contenido.style.height = "60%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginRight === "3%") {
+    contenido.style.marginRight = "13%";
+  } else {
+    contenido.style.marginRight = "3%";
+  }
+
+  ////////////NOMBRE/////////////
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.marginRight === "7%") {
+    contenido.style.marginRight = "15%";
+  } else {
+    contenido.style.marginRight = "7%";
+  }
+}
+
+////////////////////////////////
+//////////////////CASA//////////////
+/////////////////////////////////
+
+function segundotoggle() {
+  ///////TABLA/////////////
+  var contenido = document.getElementById("tabla");
+  if (contenido.style.width === "45%") {
+    contenido.style.width = "65%";
+  } else {
+    contenido.style.width = "45%";
+  }
+  if (contenido.style.marginLeft === "29%") {
+    contenido.style.marginLeft = "9%";
+  } else {
+    contenido.style.marginLeft = "29%";
+  }
+  if (contenido.style.marginTop === "7%") {
+    contenido.style.marginTop = "5%";
+  } else {
+    contenido.style.marginTop = "7%";
+  }
+
+  if (contenido.style.height === "65%") {
+    contenido.style.height = "75%";
+  } else {
+    contenido.style.height = "65%";
+  }
+
+
+
+
+  //////////FOOTER////////////////////
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.width === "40%") {
+    contenido.style.width = "60%";
+  } else {
+    contenido.style.width = "40%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.height === "60%") {
+    contenido.style.height = "80%";
+  } else {
+    contenido.style.height = "60%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginRight === "-4%") {
+    contenido.style.marginRight = "16%";
+  } else {
+    contenido.style.marginRight = "-4%";
+  }
+
+  ////////////NOMBRE/////////////
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.marginRight === "3%") {
+    contenido.style.marginRight = "11%";
+  } else {
+    contenido.style.marginRight = "3%";
+  }
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.fontSize === "100%") {
+    contenido.style.fontSize = "140%";
+  } else {
+    contenido.style.fontSize = "100%";
+  }
+
+}
+
+////////////////////////////////
+////////////////////////////////
+/////////////////////////////////
+
+function tercertoggle() {
+  ///////TABLA/////////////
+  var contenido = document.getElementById("tabla");
+  if (contenido.style.width === "45%") {
+    contenido.style.width = "65%";
+  } else {
+    contenido.style.width = "45%";
+  }
+  if (contenido.style.marginLeft === "31%") {
+    contenido.style.marginLeft = "9%";
+  } else {
+    contenido.style.marginLeft = "31%";
+  }
+  if (contenido.style.marginTop === "7%") {
+    contenido.style.marginTop = "5%";
+  } else {
+    contenido.style.marginTop = "7%";
+  }
+
+  if (contenido.style.height === "65%") {
+    contenido.style.height = "75%";
+  } else {
+    contenido.style.height = "65%";
+  }
+
+
+
+
+  //////////FOOTER////////////////////
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.width === "40%") {
+    contenido.style.width = "60%";
+  } else {
+    contenido.style.width = "40%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.height === "60%") {
+    contenido.style.height = "80%";
+  } else {
+    contenido.style.height = "60%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginRight === "0%") {
+    contenido.style.marginRight = "20%";
+  } else {
+    contenido.style.marginRight = "0%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginLeft === "8%") {
+    contenido.style.marginLeft = "4%";
+  } else {
+    contenido.style.marginLeft = "8%";
+  }
+
+  ////////////NOMBRE/////////////
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.marginRight === "1%") {
+    contenido.style.marginRight = "11%";
+  } else {
+    contenido.style.marginRight = "1%";
+  }
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.fontSize === "100%") {
+    contenido.style.fontSize = "140%";
+  } else {
+    contenido.style.fontSize = "100%";
+  }
+
+}
+
+////////////////////////////////
+////////////////////////////////
+/////////////////////////////////
+
+function cuartotoggle() {
+  ///////TABLA/////////////
+  var contenido = document.getElementById("tabla");
+  if (contenido.style.width === "45%") {
+    contenido.style.width = "65%";
+  } else {
+    contenido.style.width = "45%";
+  }
+  if (contenido.style.marginLeft === "33%") {
+    contenido.style.marginLeft = "9%";
+  } else {
+    contenido.style.marginLeft = "33%";
+  }
+  if (contenido.style.marginTop === "7%") {
+    contenido.style.marginTop = "5%";
+  } else {
+    contenido.style.marginTop = "7%";
+  }
+
+  if (contenido.style.height === "65%") {
+    contenido.style.height = "75%";
+  } else {
+    contenido.style.height = "65%";
+  }
+
+
+
+
+  //////////FOOTER////////////////////
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.width === "40%") {
+    contenido.style.width = "60%";
+  } else {
+    contenido.style.width = "40%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.height === "60%") {
+    contenido.style.height = "80%";
+  } else {
+    contenido.style.height = "60%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginRight === "0%") {
+    contenido.style.marginRight = "16%";
+  } else {
+    contenido.style.marginRight = "0%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginLeft === "12%") {
+    contenido.style.marginLeft = "0%";
+  } else {
+    contenido.style.marginLeft = "12%";
+  }
+
+  ////////////NOMBRE/////////////
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.marginRight === "-2%") {
+    contenido.style.marginRight = "11%";
+  } else {
+    contenido.style.marginRight = "-2%";
+  }
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.fontSize === "100%") {
+    contenido.style.fontSize = "140%";
+  } else {
+    contenido.style.fontSize = "100%";
+  }
+
+}
+
+
+////////////////////////////////
+////////////////////////////////
+/////////////////////////////////
+
+function quintotoggle() {
+  ///////TABLA/////////////
+  var contenido = document.getElementById("tabla");
+  if (contenido.style.width === "45%") {
+    contenido.style.width = "65%";
+  } else {
+    contenido.style.width = "45%";
+  }
+  if (contenido.style.marginLeft === "34%") {
+    contenido.style.marginLeft = "9%";
+  } else {
+    contenido.style.marginLeft = "34%";
+  }
+  if (contenido.style.marginTop === "7%") {
+    contenido.style.marginTop = "5%";
+  } else {
+    contenido.style.marginTop = "7%";
+  }
+
+  if (contenido.style.height === "65%") {
+    contenido.style.height = "75%";
+  } else {
+    contenido.style.height = "65%";
+  }
+
+
+
+  //////////FOOTER////////////////////
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.width === "40%") {
+    contenido.style.width = "60%";
+  } else {
+    contenido.style.width = "40%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.height === "60%") {
+    contenido.style.height = "80%";
+  } else {
+    contenido.style.height = "60%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginRight === "0%") {
+    contenido.style.marginRight = "16%";
+  } else {
+    contenido.style.marginRight = "0%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginLeft === "14%") {
+    contenido.style.marginLeft = "0%";
+  } else {
+    contenido.style.marginLeft = "14%";
+  }
+
+  ////////////NOMBRE/////////////
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.marginRight === "-3%") {
+    contenido.style.marginRight = "9%";
+  } else {
+    contenido.style.marginRight = "-3%";
+  }
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.fontSize === "80%") {
+    contenido.style.fontSize = "140%";
+  } else {
+    contenido.style.fontSize = "80%";
+  }
+
+}
+
+////////////////////////////////
+////////////////////////////////
+/////////////////////////////////
+
+
+function sextotoggle() {
+  ///////TABLA/////////////
+  var contenido = document.getElementById("tabla");
+  if (contenido.style.width === "45%") {
+    contenido.style.width = "65%";
+  } else {
+    contenido.style.width = "45%";
+  }
+  if (contenido.style.marginLeft === "34.8%") {
+    contenido.style.marginLeft = "9%";
+  } else {
+    contenido.style.marginLeft = "34.8%";
+  }
+  if (contenido.style.marginTop === "7%") {
+    contenido.style.marginTop = "5%";
+  } else {
+    contenido.style.marginTop = "7%";
+  }
+
+  if (contenido.style.height === "65%") {
+    contenido.style.height = "75%";
+  } else {
+    contenido.style.height = "65%";
+  }
+
+
+
+
+  //////////FOOTER////////////////////
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.width === "40%") {
+    contenido.style.width = "60%";
+  } else {
+    contenido.style.width = "40%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.height === "60%") {
+    contenido.style.height = "80%";
+  } else {
+    contenido.style.height = "60%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginRight === "0%") {
+    contenido.style.marginRight = "16%";
+  } else {
+    contenido.style.marginRight = "0%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginLeft === "14%") {
+    contenido.style.marginLeft = "0%";
+  } else {
+    contenido.style.marginLeft = "14%";
+  }
+
+  ////////////NOMBRE/////////////
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.marginRight === "-3%") {
+    contenido.style.marginRight = "9%";
+  } else {
+    contenido.style.marginRight = "-3%";
+  }
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.fontSize === "80%") {
+    contenido.style.fontSize = "140%";
+  } else {
+    contenido.style.fontSize = "80%";
+  }
+
+}
+
+
+////////////////////////////////
+////////////////////////////////
+/////////////////////////////////
+
+function septimotoggle() {
+  ///////TABLA/////////////
+  var contenido = document.getElementById("tabla");
+  if (contenido.style.width === "45%") {
+    contenido.style.width = "65%";
+  } else {
+    contenido.style.width = "45%";
+  }
+  if (contenido.style.marginLeft === "35.2%") {
+    contenido.style.marginLeft = "9%";
+  } else {
+    contenido.style.marginLeft = "35.2%";
+  }
+  if (contenido.style.marginTop === "7%") {
+    contenido.style.marginTop = "5%";
+  } else {
+    contenido.style.marginTop = "7%";
+  }
+
+  if (contenido.style.height === "65%") {
+    contenido.style.height = "75%";
+  } else {
+    contenido.style.height = "65%";
+  }
+
+
+
+  //////////FOOTER////////////////////
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.width === "40%") {
+    contenido.style.width = "60%";
+  } else {
+    contenido.style.width = "40%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.height === "60%") {
+    contenido.style.height = "80%";
+  } else {
+    contenido.style.height = "60%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginRight === "0%") {
+    contenido.style.marginRight = "16%";
+  } else {
+    contenido.style.marginRight = "0%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginLeft === "15%") {
+    contenido.style.marginLeft = "0%";
+  } else {
+    contenido.style.marginLeft = "15%";
+  }
+
+  ////////////NOMBRE/////////////
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.marginRight === "-7%") {
+    contenido.style.marginRight = "9%";
+  } else {
+    contenido.style.marginRight = "-7%";
+  }
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.fontSize === "80%") {
+    contenido.style.fontSize = "120%";
+  } else {
+    contenido.style.fontSize = "80%";
+  }
+
+}
+
+////////////////////////////////
+////////////////////////////////
+/////////////////////////////////
+
+function octavotoggle() {
+  ///////TABLA/////////////
+  var contenido = document.getElementById("tabla");
+  if (contenido.style.width === "45%") {
+    contenido.style.width = "55%";
+  } else {
+    contenido.style.width = "45%";
+  }
+  if (contenido.style.marginLeft === "45%") {
+    contenido.style.marginLeft = "10%";
+  } else {
+    contenido.style.marginLeft = "45%";
+  }
+  if (contenido.style.marginTop === "7%") {
+    contenido.style.marginTop = "10%";
+  } else {
+    contenido.style.marginTop = "7%";
+  }
+
+  if (contenido.style.height === "65%") {
+    contenido.style.height = "65%";
+  } else {
+    contenido.style.height = "65%";
+  }
+
+
+
+
+  //////////FOOTER////////////////////
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.width === "40%") {
+    contenido.style.width = "45%";
+  } else {
+    contenido.style.width = "40%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.height === "60%") {
+    contenido.style.height = "50%";
+  } else {
+    contenido.style.height = "60%";
+  }
+
+
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginRight === "0%") {
+    contenido.style.marginRight = "0%";
+  } else {
+    contenido.style.marginRight = "0%";
+  }
+
+  var contenido = document.getElementById("menu-footer");
+  if (contenido.style.marginLeft === "48%") {
+    contenido.style.marginLeft = "15%";
+  } else {
+    contenido.style.marginLeft = "48%";
+  }
+
+  ////////////NOMBRE/////////////
+
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.marginRight === "-7%") {
+    contenido.style.marginRight = "15%";
+  } else {
+    contenido.style.marginRight = "-7%";
+  }
+  var contenido = document.getElementById("nombre");
+  if (contenido.style.fontSize === "60%") {
+    contenido.style.fontSize = "80%";
+  } else {
+    contenido.style.fontSize = "60%";
+  }
+
+  ///MEDIA-PORCENTAJE////
+
+  var contenido = document.getElementById("media-porcentaje");
+  if (contenido.style.marginRight === "-50rem") {
+    contenido.style.marginRight = "0rem";
+  } else {
+    contenido.style.marginRight = "-50rem";
+  }
+  
+  var contenido = document.getElementById("media-porcentaje-fondo");
+  if (contenido.style.marginRight === "-100rem") {
+    contenido.style.marginRight = "0rem";
+  } else {
+    contenido.style.marginRight = "-100rem";
+  }
+
+
+
+ 
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

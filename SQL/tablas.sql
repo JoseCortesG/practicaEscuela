df48e0de878b03cac6d8e6fb117abdb8f08e2ad3
JoseCortesG <josecortesalc@gmail.com>
2023-05-08 10:28:54 +0200

    lunes 10.30

---
+++ SQL/tablas.sql
@@ -0,0 +1,16 @@
+
+
+/*//////////////////////////////////////////////////////////INSERCIONES/////////////////////////////////////////////////////////////////////*/
+
+INSERT INTO tipo_usuario(nombre,funciones) 
+VALUES (alumno, profesor)
+
+
+INSERT INTO usuarios(id,dni, nombre, apellidos,contraseña,direccion,telefono,tipo_usuario,usuario,activo)
+VALUES ('1234', '22222222a','Jose','Cortes Gimenez','ninguna','Calle Desengaño 21',1,'jcortesg',0);
+
+INSERT INTO asignatura(id,nombre,idProfesor1)
+VALUES('1112','Informatica',1)
+
+INSERT INTO cursos(id,nombre,)
+

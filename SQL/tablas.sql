a81675fa9947684de9b7774b7df5f710bd12d624
JoseCortesG <josecortesalc@gmail.com>
2023-05-09 12:16:45 +0200

    media día martes

--- SQL/tablas.sql
+++ SQL/tablas.sql
@@ -6,7 +6,7 @@
 /*USUARIOS*/
 INSERT INTO tipo_usuario(nombre,funciones)  VALUES 
 ("alumno", "estudiar"),
-("profesor");
+("profesor","enseñar");
 
 
 INSERT INTO usuarios(dni, nombre, apellidos,contraseña,direccion,telefono,tipoUsuario,usuario,activo)
@@ -24,8 +24,8 @@ VALUES ('22222222x','Victoria','Centenelles','ninguna','Calle Justicia 23',67789
 
 /*ASIGNATURAS*/
 
-INSERT INTO asignaturas(nombre,idProfesor1)
-VALUES('Informatica',1)
+INSERT INTO asignaturas(nombre,id_profesor1)
+VALUES('Informatica',1);
 
 
 VALUES('Matematicas',1);
@@ -49,62 +49,74 @@ VALUES('Fisica',1);
 VALUES('Geografia',1);
 
 
-VALUES('Latin',1);
-VALUES('Frances',1):
 
 /*CURSOS*/
 INSERT INTO 
 
-INSERT INTO cursos(nombre,idA1,idA2,idA3,idA4,idA5,idA6,idA7,idA8,idA8,idA9,idA10)
-VALUES('primeroEso', 1,2,3,4,5,6)
+INSERT INTO cursos(nombre,id_a1,id_a2,id_a3,id_a4,id_a5,id_a6)
+VALUES('primeroEso',1,2,3,4,5,6)
 
-INSERT INTO cursos(nombre,idA1,idA3,idA4,idA6,idA8,idA10)
-VALUES('segundoEso', 1,3,4,6,8,10)
+INSERT INTO cursos(nombre,id_a1,id_a2,id_a3,id_a4,id_a6,id_a8,)
+VALUES('segundoEso',1,2,3,4,6,8)
 
-INSERT INTO cursos(nombre,idA2,idA3,idA5,idA6,idA9,idA10)
-VALUES('terceroEso', 2,3,5,6,9,10)
+INSERT INTO cursos(nombre,id_a2,id_a3,id_a5,id_a6,id_a7,id_a8)
+VALUES('terceroEso',2,3,5,6,7,8)
 
-INSERT INTO cursos(nombre,idA1,idA4,idA5,idA7,idA9,idA10)
-VALUES('cuartoEso', 1,4,5,7,9,10)
+INSERT INTO cursos(nombre,id_a1,id_a2,id_a4,id_a5,id_a6,id_a7)
+VALUES('cuartoEso',1,2,4,5,6,7)
 
 
 /* NOTA */
 
 
-INSERT INTO notas(idAlumno,idCurso,idAsignatura,nota)
+
+
+INSERT INTO notas(id_alumno,id_curso,id_asignatura,nota)
 VALUES
 (1,4,1,10),
-(1,4,4,8),
-(1,4,5,9),
-(1,4,7,7),
-(1,4,9,9),
-(1,4,9,10)
+(1,4,2,8),
+(1,4,4,9),
+(1,4,5,7),
+(1,4,6,9),
+(1,4,7,10)
 
-INSERT INTO notas(idAlumno,idCurso,idAsignatura,nota)
+INSERT INTO notas(id_alumno,id_curso,id_asignatura,nota)
 VALUES
 (2,3,2,10),
 (2,3,3,6),
 (2,3,5,8),
 (2,3,6,7),
-(2,3,9,8),
-(2,3,10,5)
+(2,3,7,8),
+(2,3,8,5)
 
-INSERT INTO notas(idAlumno,idCurso,idAsignatura,nota)
+INSERT INTO notas(id_alumno,id_curso,id_asignatura,nota)
 VALUES
 (3,2,1,8),
-(3,2,3,5),
-(3,2,4,9),
-(3,2,6,6),
-(3,2,8,7),
-(3,2,10,10)
+(3,2,2,5),
+(3,2,3,9),
+(3,2,4,6),
+(3,2,6,7),
+(3,2,8,10)
 
-INSERT INTO notas(idAlumno,idCurso,idAsignatura,nota)
+INSERT INTO notas(id_alumno,id_curso,id_asignatura,nota)
 VALUES
 (4,1,1,9),
 (4,1,2,8),
-(4,1,3,8),
-(4,1,4,7),
-(4,1,5,6),
-(5,1,6,10)
+(4,1,4,8),
+(4,1,5,7),
+(4,1,6,6),
+(4,1,7,10)
 
 
+/*
+
+use escuela2;
+SELECT usuarios.nombre AS Alumno, cursos.nombre AS curso, asignaturas.nombre AS asignatura, notas.nota
+FROM usuarios
+JOIN notas ON usuarios.id = notas.id_alumno
+JOIN asignaturas ON notas.id_asignatura = asignaturas.id
+ JOIN cursos ON notas.id_curso = cursos.id
+WHERE usuarios.nombre = 'Jose';
+
+
+*/
